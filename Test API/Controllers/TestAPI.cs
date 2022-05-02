4238f28dfdbf985a77080f13e8e7126434e2e5f5
KICA <KICA@DESKTOP-0MT5HHM>
2022-05-02 11:47:53 +0200

    Add project files.

---
+++ Test API/Controllers/TestAPI.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Test_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestAPI : ControllerBase
+    {
+
+
+        private readonly ILogger<TestAPI> _logger;
+
+        public TestAPI(ILogger<TestAPI> logger)
+        {
+            _logger = logger;
+
+        }
+
+        //Lista objekata sa tipom Korisnik velicine 100.
+        public static List<Korisnik> lista = new List<Korisnik>(100);
+
+
+
+
+
+
+        //Get metoda koja vraca broj objekata u listi.
+        [HttpGet]
+        public int Get()
+        {
+            return lista.Count;
+        }
+
+        [HttpGet, Route("[action]")] //Primer Get poziva: localhost:port/testapi/Get2?broj1=2&broj2=3
+
+        //Get metoda koja mnozi 10 sa prvim i drugim parametrom
+        public int Get2([FromQuery] int broj1, [FromQuery] int broj2)
+        {
+            int a = 10;
+            int b = broj2;
+            int c = a * b;
+            int z = c * broj1;
+
+            return z;
+        }
+
+        //Get metoda koja vraca objekat iz liste unet preko Post metode sa odgovarajucim Id-jem u json formatu.
+        [HttpGet, Route("[action]")] //Primer Get3 poziva: localhost:port/testapi/Get3?id=1
+        public OkObjectResult Get3([FromQuery] int id)
+        {
+
+
+            return Ok(lista[id]);
+
+        }
+
+            //Post metoda koja kreira novi objekat klase Korisnik,ubacuje ga u listu i ispisuje u json formatu.
+            [HttpPost, Route("[action]")]//Primer Post poziva(Postman): localhost:port/testapi/Post?id=1&ime=milos&prezime=kicovic
+
+            public OkObjectResult Post([FromQuery] int id, [FromQuery] string ime, [FromQuery] string prezime)
+            {
+                Korisnik a = new Korisnik(id,ime,prezime);
+            lista.Insert(id, a);//Nedostatak: id mora poceti od 0 i mora ici redom,ako unesemo id 1 bez objekta sa id-jem 0 pojavice se greska.
+            return Ok(lista[id]);
+
+
+            }
+
+        
+
+
+    }
+
+    }
+
---
+++ Test API/Korisnik.cs
@@ -0,0 +1,29 @@
+﻿namespace Test_API
+{
+    public class Korisnik
+    {
+        private int id;
+        private string ime;
+        private string prezime;
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public int Id { get => id; set => id = value; }
+
+        public Korisnik(int id, string ime, string prezime)
+        {
+            Id = id;
+            Ime = ime;
+            Prezime = prezime;
+        }
+
+        public Korisnik()
+        {
+            Id = 0;
+            Ime = "";
+            Prezime = "";
+        }
+    }
+
+   
+}
